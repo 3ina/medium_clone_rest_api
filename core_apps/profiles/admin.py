612a1c06015fb6ea150549b3d183e60367391cdb
sinaroydel <3inaroydl@gmail.com>
2024-07-20 01:26:41 +0330

    add profile model to admin panel

--- core_apps/profiles/admin.py
+++ core_apps/profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core_apps.profiles.models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "id", "user", "gender", "phone_number"]
+    list_display_links = ["pkid","id","user"]
+    list_filter = ["pkid","id"]
+
+
+admin.site.register(Profile, ProfileAdmin)
