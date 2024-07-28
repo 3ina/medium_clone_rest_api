bf86ae24ba92056547a562d5833062147b8472d2
sinaroydel <3inaroydl@gmail.com>
2024-07-28 22:47:55 +0330

    add ProfileDetailAPIView

--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -3,7 +3,7 @@
 from medium_clone.settings.local import DEFAULT_FROM_EMAIL
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
-from rest_framework import generics,status
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
@@ -13,15 +13,30 @@ from rest_framework.parsers import MultiPartParser
 from .exceptions import CantFollowYourself
 from .models import Profile
 from .pagination import ProfilePagination
-from .renderers import ProfileJSONRenderer,ProfilesJSONRenderer
-from .serializers import ProfileSerializer,FollowingSerializer, \
+from .renderers import ProfileJSONRenderer, ProfilesJSONRenderer
+from .serializers import ProfileSerializer, FollowingSerializer, \
     UpdateProfileSerializer
 
-
 User = get_user_model()
+
 
 class ProfileListAPIView(generics.ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     pagination_class = ProfilePagination
     renderer_classes = (ProfilesJSONRenderer,)
+
+
+class ProfileDetailAPIView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileSerializer
+    renderer_classes = (ProfileJSONRenderer,)
+
+    def get_queryset(self):
+        queryset = Profile.objects.select_related('user')
+        return queryset
+
+    def get_object(self):
+        user = self.request.user
+        profile = self.get_queryset().get(user=user)
+        return profile
