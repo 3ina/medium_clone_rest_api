865f7f983db8fa4feb3d3e2faf0e3cd53b560a7a
sinaroydel <3inaroydl@gmail.com>
2024-07-28 22:34:24 +0330

    add ProfileListAPIView

--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# TODO: change this in production
 
-# Create your views here.
+from medium_clone.settings.local import DEFAULT_FROM_EMAIL
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from rest_framework import generics,status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
+
+from .exceptions import CantFollowYourself
+from .models import Profile
+from .pagination import ProfilePagination
+from .renderers import ProfileJSONRenderer,ProfilesJSONRenderer
+from .serializers import ProfileSerializer,FollowingSerializer, \
+    UpdateProfileSerializer
+
+
+User = get_user_model()
+
+class ProfileListAPIView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    pagination_class = ProfilePagination
+    renderer_classes = (ProfilesJSONRenderer,)
