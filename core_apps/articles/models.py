f739d00c7293de5e19c1a28fbf45549da84d2217
sinaroydel <3inaroydl@gmail.com>
2024-08-01 12:54:59 +0330

    create ArticleView model

--- core_apps/articles/models.py
+++ core_apps/articles/models.py
@@ -33,3 +33,29 @@ class Article(TimeStampedModel):
         return self.article_views.count()
 
 
+
+class ArticleView(TimeStampedModel):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="article_views"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="user_views"
+    )
+    viewer_ip = models.GenericIPAddressField(
+        verbose_name=_("viewer IP"), null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = _("Article View")
+        verbose_name_plural = _("Article Views")
+        unique_together = ("article", "user", "viewer_ip")
+
+    def __str__(self):
+        return f"{self.article.title} viewed by {self.user.first_name if self.user else 'Anonymous'} from IP {self.viewer_ip}"
+
+    @classmethod
+    def record_view(cls, article, user, viewer_ip):
+        view, _ = cls.objects.get_or_create(
+            article=article, user=user, viewer_ip=viewer_ip
+        )
+        view.save()
