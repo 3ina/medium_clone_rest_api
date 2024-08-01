905da02b2fa2184848ff1bc60e53b3bd668a7299
sinaroydel <3inaroydl@gmail.com>
2024-08-01 12:54:32 +0330

    create article model

--- core_apps/articles/models.py
+++ core_apps/articles/models.py
@@ -1,3 +1,35 @@
+from autoslug import AutoSlugField
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from taggit.managers import TaggableManager
 
-# Create your models here.
+from core_apps.common.models import TimeStampedModel
+
+from .read_time_engine import ArticleReadTimeEngine
+
+User = get_user_model()
+
+
+class Article(TimeStampedModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    title = models.CharField(verbose_name=_("Title"), max_length=255)
+    slug = AutoSlugField(populate_from="title", always_update=True, unique=True)
+    description = models.CharField(verbose_name=_("description"), max_length=255)
+    body = models.TextField(verbose_name=_("article content"))
+    banner_image = models.ImageField(
+        verbose_name=_("banner image"), default="/profile_default.png"
+    )
+    tags = TaggableManager()
+
+    def __str__(self):
+        return f"{self.author.first_name}'s article"
+
+    @property
+    def estimated_reading_time(self):
+        return ArticleReadTimeEngine.estimate_reading_time(self)
+
+    def view_count(self):
+        return self.article_views.count()
+
+
