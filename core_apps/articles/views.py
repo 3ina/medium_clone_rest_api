f2bb0a462fa233916a876f3ed546f325e81aa389
sinaroydel <3inaroydl@gmail.com>
2024-08-06 12:41:30 +0330

    add ArticleListCreateView class

--- core_apps/articles/views.py
+++ core_apps/articles/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.core.files.storage import default_storage
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics, permissions, status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+
+from .filters import ArticleFilter
+from .models import Article, ArticleView
+from .pagination import ArticlePagination
+from .permissions import IsOwnerOrReadOnly
+from .renderers import ArticleJSONRenderer, ArticlesJSONRenderer
+from .serializers import ArticleSerializer
+
+User = get_user_model()
+
+logger = logging.getLogger(__name__)
+
+
+class ArticleListCreateView(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = ArticlePagination
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_class = ArticleFilter
+    ordering_fields = [
+        "created_at",
+        "updated_at",
+    ]
+    renderer_classes = [ArticlesJSONRenderer]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        logger.info(
+            f"article {serializer.data.get('title')} created by {self.request.user.first_name}"
+        )
