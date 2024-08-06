31c55faa423bf94723e2c8ab347e704b1ac4d449
sinaroydel <3inaroydl@gmail.com>
2024-08-06 12:53:16 +0330

    add ArticleRetrieveUpdateDestroyView class

--- core_apps/articles/views.py
+++ core_apps/articles/views.py
@@ -39,3 +39,38 @@ class ArticleListCreateView(generics.ListCreateAPIView):
         logger.info(
             f"article {serializer.data.get('title')} created by {self.request.user.first_name}"
         )
+
+
+class ArticleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    lookup_field = "id"
+    renderer_classes = [ArticleJSONRenderer]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_update(self, serializer):
+        instance = serializer.save(author=self.request.user)
+        if "banner_image" in self.request.FILES:
+            if (
+                instance.banner_image
+                and instance.banner_image.name != "/profile_default.png"
+            ):
+                default_storage.delete(instance.banner_image.path)
+            instance.banner_image = self.request.FILES["banner_image"]
+            instance.save()
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Http404:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(instance)
+
+        viewer_ip = request.META.get("REMOTE_ADDR", None)
+        ArticleView.record_view(
+            article=instance, user=request.user, viewer_ip=viewer_ip
+        )
+
+        return Response(serializer.data)
