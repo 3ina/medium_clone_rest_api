e37e15b275e3ff90a968194cdc3f8ffda4aeb333
sinaroydel <3inaroydl@gmail.com>
2024-08-17 19:02:28 +0330

    add ResponseListCreateView

--- core_apps/responses/views.py
+++ core_apps/responses/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .models import Article, Response
+from .serializers import ResponseSerializer
+
+
+class ResponseListCreateView(generics.ListCreateAPIView):
+    queryset = Response.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ResponseSerializer
+
+    def get_queryset(self):
+        article_id = self.kwargs.get("article_id")
+        return Response.objects.filter(article__id=article_id, parent_response=None)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        article_id = self.kwargs.get("article_id")
+        article = get_object_or_404(Article, id=article_id)
+        serializer.save(user=user, article=article)
